b70e447260d3eef561182bb3d172079b4c8b749c
Byron <vuongbyron@yahoo.com>
2021-11-07 21:19:00 -0800

    Started Hotel class

--- src/hotel/Hotel.java
+++ src/hotel/Hotel.java
@@ -1,5 +1,14 @@
 package hotel;
 
 public class Hotel {
-
+	
+	private String name;
+	private String address;
+	private String phoneNumber;
+	
+	public Hotel(String name, String address, String phoneNumber) {
+		this.name = name;
+		this.address = address;
+		this.phoneNumber = phoneNumber;
+	}
 }
