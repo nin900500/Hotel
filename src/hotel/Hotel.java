77e733e73f0dcbd1329e2886ad038f7c52198a8b
Jimmy Phan <Jimmy.Phan@sjsu.edu>
2021-11-21 23:18:26 -0800

    rewrote user class

--- src/hotel/Hotel.java
+++ src/hotel/Hotel.java
@@ -1,25 +1,33 @@
 package hotel;
 
+import java.util.Hashtable;
+
 public class Hotel 
 {
+	
 	private String name;
 	private String address;
 	private String phoneNumber;
 	
-	public Hotel(String name, String address, String phoneNumber)
-	{
+	public Hotel(String name, String address, String phoneNumber) {
 		this.name = name;
 		this.address = address;
 		this.phoneNumber = phoneNumber;
 	}
 	
-	public static void main(String [] args)
-	{
-		System.out.print("Welcome to our hotel");
+	String getName() {
+		return this.name;
 	}
 	
+	String getAddress() {
+		return this.address;
+	}
 	
+	String getPhoneNumber() {
+		return this.phoneNumber;
+	}
 	
-	
-
+	public static void main(String [] args) {
+		System.out.print("Welcome to our hotel");
+	}
 }
